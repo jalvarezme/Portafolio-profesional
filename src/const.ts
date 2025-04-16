f8eec66232f36cebde7e8620100fb65c49a5d624
jalvarezme <jalvarezme@ucenfotec.ac.cr>
2025-04-15 20:44:52 -0600

    changing host

--- src/const.ts
+++ src/const.ts
@@ -1,14 +1,14 @@
 export const SIGNIN = "signin"
 export const SIGNUP = "signup"
 
-// export const HOST = "https://jalvarezme-appointment.deno.dev" // add env variable for this
+export const HOST = "https://jalvarezme-appointment.deno.dev" // add env variable for this
 
-export const HOST = "http://localhost:8000" // add env variable for this
+// export const HOST = "http://localhost:8000" // add env variable for this
 
 
-// export const BASE_URL = "https://jalvarezme-appointment.deno.dev/api/v1" // add env variable for this
+export const BASE_URL = "https://jalvarezme-appointment.deno.dev/api/v1" // add env variable for this
 
-export const BASE_URL = "http://localhost:8000/api/v1" // add env variable for this
+// export const BASE_URL = "http://localhost:8000/api/v1" // add env variable for this
 
 
 export const AUTH_ENDPOINTS = {
