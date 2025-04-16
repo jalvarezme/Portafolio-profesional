324d0e12320aafa9fa42f993a4f22b38e9117a8c
jalvarezme <jalvarezme@ucenfotec.ac.cr>
2025-04-15 20:06:48 -0600

    remove path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-// Import /static for a static site
-import vercelStatic from '@astrojs/vercel/static';
- 
-export default defineConfig({
-  // Must be 'static' or 'hybrid'
-  output: 'static',
-  adapter: vercelStatic(),
-});
+
+// https://astro.build/config
+export default defineConfig({});
