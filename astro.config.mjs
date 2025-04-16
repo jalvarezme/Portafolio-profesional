2af44d2dd042ee918fe13c94f38cf5dfe385be83
jalvarezme <jalvarezme@ucenfotec.ac.cr>
2025-04-15 19:34:01 -0600

    changing folder path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 // Import /static for a static site
-import vercel from '@astrojs/vercel';
-
+import vercelStatic from '@astrojs/vercel/static';
+ 
 export default defineConfig({
+  // Must be 'static' or 'hybrid'
   output: 'static',
-  adapter: vercel(),
+  adapter: vercelStatic(),
 });
